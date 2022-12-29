c7fe8bdd5c3b6eb063406e11c6561308fcf81c60
Raruto <Raruto@users.noreply.github.com>
2022-12-29 17:28:59 +0100

    add "version-" prefix for semver commands

--- Makefile.semver.mk
+++ Makefile.semver.mk
@@ -1,5 +1,5 @@
 ##
-# WARNING: do not track this file your VCS, to edit check out:
+# WARNING: do not track this file in your VCS, to edit check out:
 # 
 # <https://github.com/g3w-suite/makefiles/Makefile.semver.mk>
 ##
@@ -13,70 +13,15 @@
 # include Makefile.semver.mk
 ##
 
-ROOT_DIR ?=             $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-CURRENT_VERSION ?=      $(shell git tag --sort=v:refname | tail -1 | sed 's/^v//' )
-README_PROJECT_TITLE ?= $(notdir $(CURDIR))
+ROOT_DIR ?=        $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+CURRENT_VERSION ?= $(shell git tag --sort=v:refname | tail -1 | sed 's/^v//' )
+PROJECT_NAME ?=    $(notdir $(CURDIR))
 
 ##
 # Do nothing when `make' is run without args.
 ##
 default:
-
-##
-# Bump major:
-#
-# X.Y.Z --> X+1.0.0
-##
-major:
-	@set -e ;\
-		$(MAKE) --no-print-directory \
-			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ print "v"$$1+1, 0, 0 }' )
-
-##
-# Bump minor:
-#
-# X.Y.Z --> X.Y+1.0
-##
-minor:
-	@set -e ;\
-		$(MAKE) --no-print-directory \
-			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ print "v"$$1, $$2+1, 0 }' )
-
-##
-# Bump patch:
-#
-# X.Y.Z --> X.Y.Z+1
-##
-patch:
-	@set -e ;\
-		$(MAKE) --no-print-directory \
-			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ gsub(/[^0-9]+/, "", $$3); print "v"$$1, $$2, $$3+1 }' )
-
-##
-# Bump alpha:
-#
-# X.Y.Z-alpha.rev --> X.Y.Z-alpha.rev+1
-#
-# make alpha [ v=X.Y.Z ]
-##
-alpha:
-	@set -e ;\
-		CURRENT_VERSION=$$( [ -n "$$v" ] && echo "v"$${v#v} || echo $$CURRENT_VERSION ) ;\
-		$(MAKE) --no-print-directory \
-			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="-"}{ gsub(/[^0-9]+/, "", $$2); print "v"$$1, "alpha."($$2 == "" ? 0 : $$2+1) }' )
-
-##
-# Bump beta:
-#
-# X.Y.Z-beta.rev --> X.Y.Z-beta.rev+1
-#
-# make beta [ v=X.Y.Z ]
-##
-beta:
-	@set -e ;\
-		CURRENT_VERSION=$$( [ -n "$$v" ] && echo "v"$${v#v} || echo $$CURRENT_VERSION ) ;\
-		$(MAKE) --no-print-directory \
-			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="-"}{ gsub(/[^0-9]+/, "", $$2); print "v"$$1, "beta."($$2 == "" ? 0 : $$2+1) }' )
+	@:
 
 ##
 # Add new git version:
@@ -89,11 +34,74 @@ version:
 		echo "v$(CURRENT_VERSION)" ;\
 		echo "$$NEW_VERSION" ;\
 		if [ -n "$$NEW_VERSION" ]; then \
-			$(MAKE) --no-print-directory readme v=$$NEW_VERSION ;\
+			$(MAKE) --no-print-directory readme-version v=$$NEW_VERSION ;\
 		fi ;\
 		git add -A ;\
 		git commit -m "$$NEW_VERSION" ;\
 		git tag $$NEW_VERSION
+
+##
+# Bump major:
+#
+# X.Y.Z --> X+1.0.0
+##
+version-major:
+	@set -e ;\
+		$(MAKE) --no-print-directory \
+			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ print "v"$$1+1, 0, 0 }' )
+
+##
+# Bump minor:
+#
+# X.Y.Z --> X.Y+1.0
+##
+version-minor:
+	@set -e ;\
+		$(MAKE) --no-print-directory \
+			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ print "v"$$1, $$2+1, 0 }' )
+
+##
+# Bump patch:
+#
+# X.Y.Z --> X.Y.Z+1
+##
+version-patch:
+	@set -e ;\
+		$(MAKE) --no-print-directory \
+			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="."}{ gsub(/[^0-9]+/, "", $$3); print "v"$$1, $$2, $$3+1 }' )
+
+##
+# Bump alpha:
+#
+# X.Y.Z-alpha.rev --> X.Y.Z-alpha.rev+1
+#
+# make alpha [ v=X.Y.Z ]
+##
+version-alpha:
+	@set -e ;\
+		CURRENT_VERSION=$$( [ -n "$$v" ] && echo "v"$${v#v} || echo $$CURRENT_VERSION ) ;\
+		$(MAKE) --no-print-directory \
+			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="-"}{ gsub(/[^0-9]+/, "", $$2); print "v"$$1, "alpha."($$2 == "" ? 0 : $$2+1) }' )
+
+##
+# Bump beta:
+#
+# X.Y.Z-beta.rev --> X.Y.Z-beta.rev+1
+#
+# make beta [ v=X.Y.Z ]
+##
+version-beta:
+	@set -e ;\
+		CURRENT_VERSION=$$( [ -n "$$v" ] && echo "v"$${v#v} || echo $$CURRENT_VERSION ) ;\
+		$(MAKE) --no-print-directory \
+			version NEW_VERSION=$$( echo $(CURRENT_VERSION) | awk 'BEGIN{FS=OFS="-"}{ gsub(/[^0-9]+/, "", $$2); print "v"$$1, "beta."($$2 == "" ? 0 : $$2+1) }' )
+
+##
+# Purge local git tags (remote --> local)
+##
+prune-tags:
+	git fetch --prune origin "+refs/tags/*:refs/tags/*"
+	@echo -e '\nLocal tags deleted\n'
 
 ##
 # Update remote git tags (local --> remote)
@@ -104,17 +112,10 @@ push-tags:
 	@echo -e '\nRemote tags updated\n'
 
 ##
-# Purge local git tags (remote --> local)
-##
-prune-tags:
-	git fetch --prune origin "+refs/tags/*:refs/tags/*"
-	@echo -e '\nLocal tags deleted\n'
-
-##
 # Update readme version
 #
 # make readme [ v=X.Y.Z ]
 ##
-readme:
-	@sed -i "1c\\# $(README_PROJECT_TITLE) $(v)" $(ROOT_DIR)/README.md
-	@echo -e '\nReadme heading updated: # $(README_PROJECT_TITLE) $(v)\n'
+readme-version:
+	@sed -i "1c\\# $(PROJECT_NAME) $(v)" $(ROOT_DIR)/README.md
+	@echo -e '\nReadme heading updated: # $(PROJECT_NAME) $(v)\n'
